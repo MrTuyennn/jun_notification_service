622efcd73b7b4bce71802880cedb9cc10bc94844
Tuyênnn <55830905+MrTuyennn@users.noreply.github.com>
2025-04-25 11:36:43 +0700

    implement medthod IOS notification

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -5,23 +5,63 @@ import UIKit
 @objc class AppDelegate: FlutterAppDelegate {
   
     let center = UNUserNotificationCenter.current()
+    var deviceToken : String = ""
     
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-      self.registerForPushNotifications()
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        
+        let pushNotificationChannel = FlutterMethodChannel(name: AppConstants.CHANNEL_NOTIFICATION, binaryMessenger: controller.binaryMessenger)
+        
+        pushNotificationChannel
+            .setMethodCallHandler{ [weak self] (
+                call: FlutterMethodCall,
+                result: @escaping FlutterResult
+            ) in
+                switch call.method {
+                case AppConstants.REQUEST_NOTIFICATION_PERMISSION:
+                    self?.registerForPushNotifications()
+                case AppConstants.REGISTER_FOR_PUSH_NOTIFICATION:
+                    self?.registerForPushNotifications(application: application, result: result)
+                case AppConstants.RETRIEVEDEVICETOKEN:
+                    self?.getDeviceToken(result: result)
+                default:
+                    result(FlutterMethodNotImplemented)
+                }
+            }
+        
+        
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+    }
+    
+    private func registerForPushNotifications(application: UIApplication, result: @escaping FlutterResult) {
+        application.registerForRemoteNotifications()
+        result("Device Token registration initiated")
+    }
+    
+    private func getDeviceToken(result: @escaping FlutterResult) {
+        if(deviceToken.isEmpty){
+            result(FlutterError(code: "UNAVAILABLE", message: "Device token not available", details: nil))
+        } else{
+            result(deviceToken)
+        }
+    }
+    
     
     func registerForPushNotifications() {
         center.delegate = self
         center.requestAuthorization(options: [.alert,.sound,.badge,]) {
-                [weak self](granted,error) in
-                guard let self = self else { return }
-                self.getNotificationSettings()
-            }
+            [weak self](granted,error) in
+            guard let self = self else { return }
+            self.getNotificationSettings()
+        }
     }
 
     func getNotificationSettings() {
@@ -80,14 +120,14 @@ import UIKit
     private func handleNotification(userInfo: [AnyHashable: Any]) {
         //
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-//        let pushNotificationChannel = FlutterMethodChannel(name: AppConstants.CHANNEL_NOTIFICATION,binaryMessenger: controller.binaryMessenger)
-//        
-//        
-//        if let aps = userInfo["aps"] as? [String: Any],
-//           let customData = aps["customData"] as? String {
-//            print("----- debug ----- handle Notfi \(customData)")
-//            pushNotificationChannel.invokeMethod(AppConstants.METHOD_PUSH_NOTIFICATION, arguments: customData)
-//        }
+        //        let pushNotificationChannel = FlutterMethodChannel(name: AppConstants.CHANNEL_NOTIFICATION,binaryMessenger: controller.binaryMessenger)
+        //        
+        //        
+        //        if let aps = userInfo["aps"] as? [String: Any],
+        //           let customData = aps["customData"] as? String {
+        //            print("----- debug ----- handle Notfi \(customData)")
+        //            pushNotificationChannel.invokeMethod(AppConstants.METHOD_PUSH_NOTIFICATION, arguments: customData)
+        //        }
         
     }
 }
---
+++ ios/Runner/Utils/AppConstants.swift
@@ -0,0 +1,15 @@
+//
+//  AppConstants.swift
+//  Runner
+//
+//  Created by Nguyen Ngoc Tuyen on 25/4/25.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let CHANNEL_NOTIFICATION = "channelNotification"
+    static let REQUEST_NOTIFICATION_PERMISSION = "requestNotificationPermission"
+    static let REGISTER_FOR_PUSH_NOTIFICATION = "registerForPushNotifications"
+    static let RETRIEVEDEVICETOKEN = "retrieveDeviceToken"
+}
